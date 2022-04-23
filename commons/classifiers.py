c8c397fd7330abdfe45f4d43e2166ec32d455ebd
Rodrigo Parente <digus.parente@gmail.com>
2022-04-23 14:59:49 -0300

    Added the intel sklearnex lib to speed up the tests

--- commons/classifiers.py
+++ commons/classifiers.py
@@ -1,12 +1,17 @@
 # third-party imports
 import numpy as np
 
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
+# patching sklearn lib to run faster
+# https://intel.github.io/scikit-learn-intelex/what-is-patching.html
+from sklearnex import patch_sklearn
+patch_sklearn()
+
+from sklearn.ensemble import GradientBoostingClassifier  # noqa E402
+from sklearn.linear_model import LogisticRegression  # noqa E402
+from sklearn.ensemble import RandomForestClassifier  # noqa E402
+from sklearn.model_selection import GridSearchCV  # noqa E402
+from sklearn.neural_network import MLPClassifier  # noqa E402
+from sklearn.svm import SVC  # noqa E402
 
 
 def get_estimator(name):
@@ -16,13 +21,13 @@ def get_estimator(name):
         return GradientBoostingClassifier()
     elif name == 'lr':
         return LogisticRegression(penalty='none')
-    elif name == 'svc':
-        return GridSearchCV(SVC(probability=True, max_iter=2000000), [
+    elif name == 'svc-with-grid':
+        return GridSearchCV(SVC(probability=True, random_state=42), [
             {'kernel': ['rbf'], 'gamma': 2 ** np.arange(-15.0, 4.0, 2),
                 'C': 2 ** np.arange(-5.0, 16.0, 2)},
             {'kernel': ['linear'], 'C': 2 ** np.arange(-5.0, 16.0, 2)}
-        ], scoring='accuracy', cv=5)
-    elif name == 'svc-linear':
-        return SVC(kernel='linear', C=2, probability=True, max_iter=2000000)
+        ], scoring='accuracy', cv=5, n_jobs=-1)
+    elif name == 'svc':
+        return SVC(kernel='linear', C=2, probability=True)
     elif name == 'mlp':
         return MLPClassifier()
