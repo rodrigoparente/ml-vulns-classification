694574c00545c192979349c9916b6b22935b3da9
Rodrigo Parente <digus.parente@gmail.com>
2022-07-05 18:01:13 -0300

    Add variables to execute file

--- execute.py
+++ execute.py
@@ -11,38 +11,44 @@ from constants import CLASSIFIERS
 
 def execute():
 
+    INITIAL_SIZE = 20
+    TEST_SIZE = 40
+    N_REPETITIONS = 30
+    N_QUERIES = 80
+    N_COMMITTEE = 2
+
     print('Executing active learning query & committee strategies tests...')
     query_committee(classifiers=CLASSIFIERS,
-                    initial_size=20,
-                    test_size=40,
-                    n_repetitions=30,
-                    n_queries=80,
-                    n_committee=2)
+                    initial_size=INITIAL_SIZE,
+                    test_size=TEST_SIZE,
+                    n_repetitions=N_REPETITIONS,
+                    n_queries=N_QUERIES,
+                    n_committee=N_COMMITTEE)
 
     print('Executing active semi-supervised learning tests...')
     active_semi(classifiers=CLASSIFIERS,
-                initial_size=20,
-                test_size=40,
-                n_repetitions=30,
-                n_queries=80)
+                initial_size=INITIAL_SIZE,
+                test_size=TEST_SIZE,
+                n_repetitions=N_REPETITIONS,
+                n_queries=N_QUERIES)
 
     print('Executing active supervised learning tests...')
     active_super(classifiers=CLASSIFIERS,
-                 initial_size=20,
-                 test_size=40,
-                 n_repetitions=30,
-                 n_queries=80)
+                 initial_size=INITIAL_SIZE,
+                 test_size=TEST_SIZE,
+                 n_repetitions=N_REPETITIONS,
+                 n_queries=N_QUERIES)
 
     print('Executing random supervised learning tests...')
     random_super(classifiers=CLASSIFIERS,
-                 initial_size=20,
-                 test_size=40,
-                 n_repetitions=30,
-                 n_queries=80)
+                 initial_size=INITIAL_SIZE,
+                 test_size=TEST_SIZE,
+                 n_repetitions=N_REPETITIONS,
+                 n_queries=N_QUERIES)
 
     print('Executing random semi-supervised learning tests...')
     random_semi(classifiers=CLASSIFIERS,
-                initial_size=20,
-                test_size=40,
-                n_repetitions=30,
-                n_queries=80)
+                initial_size=INITIAL_SIZE,
+                test_size=TEST_SIZE,
+                n_repetitions=N_REPETITIONS,
+                n_queries=N_QUERIES)
