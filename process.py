Fixed std calculation formula

process.py
@@ -6,7 +6,6 @@ from commons.file import read_file
 from commons.file import to_file
 from commons.file import fmt_list
 from commons.file import calculate_mean_matrix
-from commons.confidence import interval
 
 # local imports
 from constants import TECHNIQUES
@@ -19,27 +18,25 @@ from constants import OUTPUT_FILE_PATH
 
 def calculate_overall_score(tech, files):
     for name, values in files.items():
+
+        # mean and std
         mean_list = list()
-        lower_list = list()
-        upper_list = list()
+        std_list = list()
 
         for col_index in range(len(values[0])):
             col_elems = [row[col_index] for row in values]
 
             mean = np.mean(col_elems)
-            lower, upper = interval(col_elems)
+            std = np.std(col_elems)
 
             mean_list.append(mean)
-            lower_list.append(lower)
-            upper_list.append(upper)
+            std_list.append(std)
 
         mean = fmt_list(mean_list)
-        lower = fmt_list(lower_list)
-        upper = fmt_list(upper_list)
+        std = fmt_list(std_list)
 
         to_file(f'{OUTPUT_FILE_PATH}/{tech}-{name}-mean.txt', f'{mean}\n')
-        to_file(f'{OUTPUT_FILE_PATH}/{tech}-{name}-lower.txt', f'{lower}\n')
-        to_file(f'{OUTPUT_FILE_PATH}/{tech}-{name}-upper.txt', f'{upper}\n')
+        to_file(f'{OUTPUT_FILE_PATH}/{tech}-{name}-std.txt', f'{std}\n')
 
 
 def calculate_cm_score(tech, cms):
@@ -79,14 +76,13 @@ def process():
                 'f1s': read_file(f'{base_url}/{learner}-f1.txt')
             }
 
-            # calculating mean, lower and upper values for
+            # calculating mean and std values for
             # acc, precision, recall, f1 and learning time
             calculate_overall_score(tech, files)
 
             cms = read_file(f'{base_url}/{learner}-cm.txt', cast=str, delimiter='-')
 
-            # calculating mean, lower
-            # and upper confusion matrices
+            # calculating mean and std confusion matrices
             calculate_cm_score(tech, cms)
 
     # compile results for active learning query & committee strategies
@@ -104,12 +100,11 @@ def process():
                     'f1s': read_file(f'{base_url}/{learner}-{strategy}-f1.txt')
                 }
 
-                # calculating mean, lower and upper values for
+                # calculating mean and std values for
                 # acc, precision, recall, f1 and learning time
                 calculate_overall_score(f'{query_name}-{strategy}', file_dict)
 
                 cms = read_file(f'{base_url}/{learner}-{strategy}-cm.txt', cast=str, delimiter='-')
 
-                # calculating mean, lower
-                # and upper confusion matrices
+                # calculating mean and std confusion matrices
                 calculate_cm_score(f'{query_name}-{strategy}', cms)